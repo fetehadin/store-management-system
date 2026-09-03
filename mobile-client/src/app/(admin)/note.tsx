d1dd74812504cf6d16806e95e141431cb7584758
fetehadin negash <fetehadinnegash@gmail.com>
2026-09-03 12:38:02 +0300

    fix(admin): update note screen wrapper function name

--- mobile-client/src/app/(admin)/note.tsx
+++ mobile-client/src/app/(admin)/note.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import SharedNotes from '../../components/SharedNotes';
 
-export default function NotesScreen() {
+export default function AdminNotesScreen() {
   return <SharedNotes />;
 }
